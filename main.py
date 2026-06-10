89c0380ac0ae80f92dceed1e2a6ceca45d11c91b
bru.oliveraa <brunactpm2005@gmail.com>
2026-06-09 21:49:23 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# realizando a importação do pygame
+import pygame
+
+pygame.init() # Inicializa o pacote do pygame;
+
+window = pygame.display.set_mode(size=(800, 600)) # Realizada a criação de uma variável com tamanho da tela que irá aparecer para o jogador;
+
+while True:
+# Check for all events;
+ for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+         pygame.quit() # Close window;
+         quit() # End pygame
+
