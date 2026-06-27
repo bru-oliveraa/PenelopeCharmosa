a027b021362369b7f4528cc2cb08a706b63666b6
bru.oliveraa <brunactpm2005@gmail.com>
2026-06-27 15:40:16 -0300

    Adding classes generated from StarUML

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.Window = None
+        self.Name = None
+        self.Entity_list = None
+
+    def run(self, ):
+        pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 pygame.init() # Inicializa o pacote do pygame;
 
-window = pygame.display.set_mode(size=(800, 600)) # Realizada a criação de uma variável com tamanho da tela que irá aparecer para o jogador;
+window = pygame.display.set_mode(size=(800, 600)) # Realiza a criação de uma variável com tamanho da tela que irá aparecer para o jogador;
 
 while True:
 # Check for all events;
